a3dbafcf4a4dde9e1337cfdf7a655674524ee9a0
Gabrielle <ms.grodarte@gmail.com>
2024-05-13 11:58:58 -0700

    complete lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,39 @@
 // Your tests here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", ()=>{
+    it("returns true if word reads same forwards and backwards", ()=>{
+        const word = "racecar"
+        const palindrome = isPalindrome(word)
+
+        expect(palindrome).toBe(true)
+    })
+    it("returns false if word reads different forwards and backwards", ()=>{
+        const word = "car"
+        const palindrome = isPalindrome(word)
+
+        expect(palindrome).toBe(false)
+    })
+    it("handles uppercase and lowercase letters", ()=>{
+        const word = "rAcEcaR"
+        const palindrome = isPalindrome(word)
+        expect(palindrome).toBe(true)
+    })
+    it("returns false for an empty string", ()=>{
+        const word = ""
+        const palindrome = isPalindrome(word)
+
+        expect((palindrome)).toBe(false)
+    })
+    it("throws an error if input has any non-alphabetical characters", ()=>{
+        expect(()=>isPalindrome("123")).toThrow()
+        expect(()=>isPalindrome("Hello, world!")).toThrow()
+        expect(()=>isPalindrome("race car!")).toThrow()
+    })
+    it("throws an error if input is not a string", ()=>{
+        expect(()=>isPalindrome(1111)).toThrow()
+        expect(()=>isPalindrome(null)).toThrow()
+        expect(()=>isPalindrome(true)).toThrow()
+
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,12 @@
 // Your code here
+export function isPalindrome(word){
+    if(word.length === 0){
+        return false
+    } else if (!/^[A-Za-z]+$/.test(word)){
+        throw new Error("Word contains special characters")
+    }
+
+    const backWord = word.split("").reverse().join("")
+    
+    return backWord.toLowerCase() === word.toLowerCase()
+}
